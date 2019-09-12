9cb16963d5efbd984ab3e93b3b7b2400e306e4b1
Ogunsola-Tumininu <tumininu.ogunsola@metricinternet.com>
2019-09-12 14:33:52 +0100

    make it better

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var port = process.env.PORT || 8080;
 const app = express();
 
 //Set static folder
-app.use(express.static( '/dist/flightapp'));
+app.use(express.static( 'dist/flightapp'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/flightapp/index.html'))
+    res.sendFile(path.join(__dirname, 'dist/flightapp/index.html'))
 })
 
 app.listen(port, () => console.log('Express server running on port ' + port));
