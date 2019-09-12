e6079e6efd6c25f78a01aa7dc64f2e168ebd985f
Ogunsola-Tumininu <tumininu.ogunsola@metricinternet.com>
2019-09-12 15:03:02 +0100

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+var cors = require('cors');
 
 var port = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors());
+
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'dist/flightapp')));
