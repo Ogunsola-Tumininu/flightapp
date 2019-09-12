9115d5d41767ad58a460430e31cb519d42839588
Ogunsola-Tumininu <tumininu.ogunsola@metricinternet.com>
2019-09-12 14:40:45 +0100

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 const app = express();
 
 //Set static folder
-app.use(express.static( 'dist/flightapp'));
+app.use(express.static(path.join(__dirname, 'dist/flightapp')));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/flightapp/index.html'))
